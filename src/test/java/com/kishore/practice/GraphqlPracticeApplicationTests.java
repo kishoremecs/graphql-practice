9c307056d8ba49cd06e78c88d3ffa2a13ad2bbc0
Kishore Kumar <kmr.kishore@gmail.com>
2022-08-22 22:45:27 +0530

    Adding Initial Checkin for GraphQL Java Spring Boot Integration.

---
+++ src/test/java/com/kishore/practice/GraphqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
